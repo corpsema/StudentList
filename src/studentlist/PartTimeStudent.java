8e458a201d1a4d1c60b432fb260d72fae9fd6bd9
mo-a114 <73707049+mo-a114@users.noreply.github.com>
2021-01-25 10:59:56 -0500

    Part time student

---
+++ src/studentlist/PartTimeStudent.java
@@ -0,0 +1,19 @@
+package studentlist;
+
+public class PartTimeStudent extends Student {
+
+    private int numCourses;
+
+    public PartTimeStudent(String name, int numCourses) {
+        super(name);
+        this.numCourses = numCourses;
+    }
+
+    public int getNumCourses() {
+        return numCourses;
+    }
+
+    public void setNumCourses(int numCourses) {
+        this.numCourses = numCourses;
+    }
+}
